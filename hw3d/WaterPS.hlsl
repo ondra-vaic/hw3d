ed446066854142e0b459af3964ff69a0a91d72ea
ondra-vaic <Ondra.vaic@gmail.com>
2022-04-12 18:28:10 +0200

    beter phong and water color

--- hw3d/WaterPS.hlsl
+++ hw3d/WaterPS.hlsl
@@ -19,13 +19,17 @@ float4 main(float4 Position : SV_Position, float4 WorldPosition : Position, floa
 {
 	float3 normal = normalize(Normal);
 	float3 lightDirection = normalize(lightPos - WorldPosition);
-	float3 lightColor = float3(0.6, 0.6, 0.6);
+	float3 lightColor = float3(0.7, 0.83, 0.93);
+	float3 ambient = float3(0.15, 0.15, 0.15);
+	float3 waterColor = float3(0.05, 0.36, 0.61);
 
-	float diffuse = dot(normal, lightDirection);
+	float diffuse = saturate(dot(normal, lightDirection));
 
 	float3 cameraToPosition = normalize(cameraPosition - WorldPosition.xyz);
 	float3 R = reflect(lightDirection, normal);
-	float specular = 0.4f * pow(saturate(dot(R, -cameraToPosition)), 40.5f);
+	float specular = 0.2f * pow(saturate(dot(R, -cameraToPosition)), 80.5f);
 
-	return float4(lightColor * diffuse + specular, 1);
+	float3 diffuseTotal = lightColor * diffuse * diffuseIntensity;
+
+	return float4((diffuseTotal + ambient) * waterColor + specular, 1);
 }
